f80950c5c4b1dcb678ee7c47bc1e1926081e402a
HiIAm <2253608218@qq.com>
2021-05-13 22:00:39 +0800

    jQuery 发送请求

--- server.js
+++ server.js
@@ -64,6 +64,16 @@ app.get('/delay',(request,response)=>{
     },3000)
 });
 
+//jQuery 服务
+app.all('/jquery-server',(request,response)=>{
+    //设置响应头 设置允许跨域
+    response.setHeader("Access-Control-Allow-Origin","*")
+    response.setHeader("Access-Control-Allow-Headers","*")
+    // response.send("Hello jQuery AJAX");
+    const data={"name":"尚硅谷"};
+    response.send(JSON.stringify(data));
+});
+
 //4.监听端口启动服务
 app.listen(8000,()=>{
     console.log("服务已经启动，8080端口监听中");
